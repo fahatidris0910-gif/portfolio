f7b600a52a6f40ca48e3062fe8e9727e0a745649
Fahat Idris <fahatidris0910@gmail.com>
2026-08-22 23:16:57 +0800

    initial portfolio

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
